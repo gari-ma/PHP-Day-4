a93804b38ed286be3733e6723225b438ba520643
npgarima <thegarimaweb@gmail.com>
2023-04-02 13:19:48 +0545

    initial commit

---
+++ Javascripts/script.js
@@ -0,0 +1,10 @@
+ var first = document.getElementById("First");
+ first.innerHTML= " hello";
+
+ var second = document.getElementsByClassName("garima");
+
+ second[0].innerHTML="hello whatsup";
+ second[2].innerHTML="hello whatsup";
+
+ second[0].style.color="red";
+first.style.color="red";
---
+++ form.js
@@ -0,0 +1,3 @@
+var name=document.getElementById("name")
+var age=document.getElementById("age")
+var submit=document.getElementById("submit")
